921cde3af50b67ea1210dfad7fdbbd4f48c69837
Peter Kim <kroits09@gmail.com>
2018-05-16 12:45:02 +0900

    Retrieve necessary data from Twitter API

--- js/app.js
+++ js/app.js
@@ -1,2 +1,95 @@
 const express = require('express');
 const app = express();
+const Twit = require('twit')
+const config = require('./config.js');
+const moment = require('moment');
+
+const T = new Twit(config); 
+
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const twitterData = {
+    tweets: [],
+    friends: [],
+    messages: []
+}
+// Each rendered result must include all of the information seen in the sample layout:
+    // Tweets
+        // tweet content
+        // # of retweets
+        // # of likes
+        // date tweeted
+    // Friends
+        // profile image
+        // real name
+        // screen name
+    // Messages
+        // message body
+        // date the message was sent
+        // time the message was sent
+
+T.get('statuses/user_timeline', { count: 5 }, (err, data, response) => {
+    if (err) {
+        err.message = 'Sorry, an Error occurred while retrieving the tweets.';
+        console.log('an Error occurred while retrieving the tweets');
+        return next(err);
+    }
+    
+    data.forEach((tweetInfo) => {
+        let tweetObj = {};
+        tweetObj.content = tweetInfo.text;
+        tweetObj.retweets = tweetInfo.retweet_count;
+        tweetObj.likes = tweetInfo.favorite_count;
+        let date = new Date(tweetInfo.created_at);
+        tweetObj.date = date.toDateString();
+        twitterData.tweets.push(tweetObj);
+    });
+    console.log(twitterData.tweets);
+});
+
+T.get('friends/list', { count: 5}, (err, data, response) => {
+    if (err) {
+        err.message = 'Sorry, an Error occurred while retrieving the friends list.';
+        console.log('an Error occurred while retrieving the friends list');
+        return next(err);
+    }
+
+    data.users.forEach((friendsInfo) => {
+        let friendsObj = {};
+        friendsObj.profileImage = friendsInfo.profile_image_url;
+        friendsObj.name = friendsInfo.name;
+        friendsObj.screenName = friendsInfo.screen_name;
+        twitterData.friends.push(friendsObj);
+    });
+    console.log(twitterData.friends);
+});
+
+T.get('direct_messages/events/list', { count: 5}, (err, data, response) => {
+    if (err) {
+        err.message = 'Sorry, an Error occurred while retrieving the direct messages.';
+        console.log('an Error occurred while retrieving the direct messages');
+        return next(err);
+    }
+
+    data.events.forEach((messageInfo) => {
+        let messageObj = {};
+        messageObj.text = messageInfo.message_create.message_data.text;
+        let timestamp = parseInt(messageInfo.created_timestamp);
+        let date = moment(timestamp).format('YYYY-MM-DD');
+        let time = moment(timestamp).format('h:mm:ss a');
+        messageObj.date = date;
+        messageObj.time = time;
+        twitterData.messages.push(messageObj);
+    });
+    console.log(twitterData.messages);
+});
+
+app.listen(3000, () => {
+    console.log('The app is running on the localhost:3000!'); 
+});
